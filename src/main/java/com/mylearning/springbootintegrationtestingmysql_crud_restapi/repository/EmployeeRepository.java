a2f17a6fcffac19ac697e6dba29934abd3cf4441
MonuDevJP <114223547+MonuDevJP@users.noreply.github.com>
2022-09-23 19:01:00 +0530

    Update EmployeeRepository.java
    findByFirstName method added

--- src/main/java/com/mylearning/springbootintegrationtestingmysql_crud_restapi/repository/EmployeeRepository.java
+++ src/main/java/com/mylearning/springbootintegrationtestingmysql_crud_restapi/repository/EmployeeRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String email);
+    
+    Optional<Employee> findByFirstName(String firstName);
 
     Optional<Employee> findByFirstNameContainingIgnoreCase(String firstName);
 }

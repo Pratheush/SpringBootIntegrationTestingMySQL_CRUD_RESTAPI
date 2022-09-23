efa28b9c18c83b64707045daedf632924c9e29de
pratheush <pratheush@outlook.com>
2022-09-23 13:36:45 +0530

    Third Commit repo annotate with @Repository

--- src/main/java/com/mylearning/springbootintegrationtestingmysql_crud_restapi/repository/EmployeeRepository.java
+++ src/main/java/com/mylearning/springbootintegrationtestingmysql_crud_restapi/repository/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.mylearning.springbootintegrationtestingmysql_crud_restapi.repository
 
 import com.mylearning.springbootintegrationtestingmysql_crud_restapi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String email);
